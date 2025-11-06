7e51e41baeff9e956d13cc6da0c0ad0cadb223a3
Lyonel Pierce <77861061+lyonelpierce@users.noreply.github.com>
2025-11-06 09:13:36 -0500

    convex better auth

---
+++ src/routes/__root.tsx
@@ -0,0 +1,116 @@
+import {
+  HeadContent,
+  Scripts,
+  createRootRouteWithContext,
+  useRouteContext,
+  Outlet,
+} from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { TanStackDevtools } from '@tanstack/react-devtools'
+import { ConvexReactClient } from 'convex/react'
+import Header from '../components/Header'
+import { ConvexQueryClient } from '@convex-dev/react-query'
+import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
+import { ConvexBetterAuthProvider } from '@convex-dev/better-auth/react'
+import ConvexProvider from '../integrations/convex/provider'
+import { authClient } from "@/lib/auth-client";
+import appCss from '../styles.css?url'
+import { createServerFn } from '@tanstack/react-start'
+import type { QueryClient } from '@tanstack/react-query'
+import { fetchSession, getCookieName } from '@convex-dev/better-auth/react-start'
+import { getCookie, getRequest } from '@tanstack/react-start/server'
+
+interface MyRouterContext {
+  queryClient: QueryClient
+  convexClient: ConvexReactClient
+  convexQueryClient: ConvexQueryClient
+}
+
+const fetchAuth = createServerFn({ method: 'GET' }).handler(async () => {
+  const { createAuth } = await import('../../convex/auth')
+  const { session } = await fetchSession(getRequest())
+  const sessionCookieName = getCookieName(createAuth)
+  const token = getCookie(sessionCookieName)
+  return {
+    userId: session?.user.id,
+    token,
+  }
+})
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
+  head: () => ({
+    meta: [
+      {
+        charSet: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        title: 'TanStack Start Starter',
+      },
+    ],
+    links: [
+      {
+        rel: 'stylesheet',
+        href: appCss,
+      },
+    ],
+  }),
+  beforeLoad: async (ctx) => {
+    // all queries, mutations and action made with TanStack Query will be
+    // authenticated by an identity token.
+    const { userId, token } = await fetchAuth()
+    // During SSR only (the only time serverHttpClient exists),
+    // set the auth token to make HTTP queries with.
+    if (token) {
+      ctx.context.convexQueryClient.serverHttpClient?.setAuth(token)
+    }
+    return { userId, token }
+  },
+  component: RootComponent,
+})
+
+function RootComponent() {
+  const context = useRouteContext({ from: Route.id })
+  return (
+    <ConvexBetterAuthProvider
+      client={context.convexClient}
+      authClient={authClient}
+    >
+      <RootDocument>
+        <Outlet />
+      </RootDocument>
+    </ConvexBetterAuthProvider>
+  )
+}
+
+function RootDocument({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        <ConvexProvider>
+          <Header />
+          {children}
+          <TanStackDevtools
+            config={{
+              position: 'bottom-right',
+            }}
+            plugins={[
+              {
+                name: 'Tanstack Router',
+                render: <TanStackRouterDevtoolsPanel />,
+              },
+              TanStackQueryDevtools,
+            ]}
+          />
+        </ConvexProvider>
+        <Scripts />
+      </body>
+    </html>
+  )
+}
